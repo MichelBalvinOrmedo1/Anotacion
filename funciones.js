f5e01046a212c5a50002c3461205c2b42e3a2e34
MichelBalvinOrmedo1 <michelbalvinor@gmail.com>
2023-01-23 09:08:49 -0500

    eje

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function sumaDeTres(x){
+    console.log(x + 3)
+}
+sumaDeTres(5)
+var sumaDeTres1= function(x){
+    return x +3
+}
+console.log(sumaDeTres1(2))
+
+var nombre = (nombre) => { 
+    return'Hola ' + nombre};
+console.log(nombre('michel'))
+console.log('HOla PErra'    )
